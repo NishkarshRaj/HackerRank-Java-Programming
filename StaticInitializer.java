d34f6d5436114d90e82264fd0c3e24269ea62181
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-06-18 21:24:49 +0530

    Static Block Initializer code added

---
+++ StaticInitializer.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+static int B,H;
+static boolean flag;
+//static block initialization
+static
+{   
+    Scanner reader = new Scanner(System.in);
+    try
+    {
+        B = reader.nextInt();
+        H = reader.nextInt();
+        if((B<0)||(H<0))
+        {
+            throw new Exception("Breadth and height must be positive");
+        }
+        flag=true;
+    }
+    catch(Exception e)
+    {
+        System.out.println(e);
+    }
+}
+public static void main(String[] args){
+		if(flag){ //unline c/cpp integer not acceptable like 1 is true only boolean conditions
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
